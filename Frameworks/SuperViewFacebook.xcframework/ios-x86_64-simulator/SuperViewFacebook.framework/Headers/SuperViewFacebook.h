3d8a9f330c04ac897ca7f1d2f746bff07a3a0668
Brommko LLC <brommko@yahoo.com>
2020-12-10 21:18:36 +0100

    -xcframework

---
+++ Frameworks/SuperViewFacebook.xcframework/ios-x86_64-simulator/SuperViewFacebook.framework/Headers/SuperViewFacebook.h
@@ -0,0 +1,19 @@
+//
+//  SuperViewFacebook.h
+//  SuperViewFacebook
+//
+//  Created by Brommko LLC on 19/03/2019.
+//  Copyright © 2019 Brommko LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SuperViewFacebook.
+FOUNDATION_EXPORT double SuperViewFacebookVersionNumber;
+
+//! Project version string for SuperViewFacebook.
+FOUNDATION_EXPORT const unsigned char SuperViewFacebookVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SuperViewFacebook/PublicHeader.h>
+
+
---
+++ Frameworks/SuperViewLocation.xcframework/ios-arm64/SuperViewLocation.framework/Headers/SuperViewLocation.h
@@ -0,0 +1,19 @@
+//
+//  SuperViewLocation.h
+//  SuperViewLocation
+//
+//  Created by Brommko LLC on 28/03/2019.
+//  Copyright © 2019 Brommko LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SuperViewLocation.
+FOUNDATION_EXPORT double SuperViewLocationVersionNumber;
+
+//! Project version string for SuperViewLocation.
+FOUNDATION_EXPORT const unsigned char SuperViewLocationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SuperViewLocation/PublicHeader.h>
+
+
