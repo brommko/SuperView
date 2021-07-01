a872fc9be4ef5e411d44e7b3864dfa8b94efb561
Brommko LLC <44063941+brommko@users.noreply.github.com>
2021-07-01 09:52:15 +0200

    - update (#9)
    - update

---
+++ Frameworks/SuperViewAdMob.xcframework/ios-arm64/SuperViewAdMob.framework/Frameworks/SuperViewCore.framework/www.bundle/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    var iOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
+    var $geoLocation = $("#geo-location");
+    if (!$geoLocation) return;
+    if (iOS) {
+        $geoLocation.attr("href", "comgooglemaps://?center=40.765819,-73.975866&zoom=14&views=traffic")
+        $geoLocation.text("comgooglemaps://?center=40.765819,-73.975866")
+    } else {
+        $geoLocation.attr("href", "geo:40.765819,-73.975866")
+        $geoLocation.text("geo:40.765819,-73.975866")
+    }
+});
