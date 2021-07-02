89e834d1830172fcc686d633b8ad29a2df8f9fa0
Brommko LLC <44063941+brommko@users.noreply.github.com>
2021-07-02 14:11:44 +0200

    - update (#11)
    - update

---
+++ Pods/SuperView/Frameworks/SuperViewFirebase.xcframework/ios-arm64/SuperViewFirebase.framework/Headers/SuperViewFirebase.h
@@ -0,0 +1,19 @@
+//
+//  SuperViewFirebase.h
+//  SuperViewFirebase
+//
+//  Created by Brommko LLC on 16/09/2019.
+//  Copyright © 2019 Brommko LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SuperViewFirebase.
+FOUNDATION_EXPORT double SuperViewFirebaseVersionNumber;
+
+//! Project version string for SuperViewFirebase.
+FOUNDATION_EXPORT const unsigned char SuperViewFirebaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SuperViewFirebase/PublicHeader.h>
+
+
---
+++ Pods/SuperView/Frameworks/SuperViewQR.xcframework/ios-x86_64-simulator/SuperViewQR.framework/Headers/SuperViewQR.h
@@ -0,0 +1,19 @@
+//
+//  SuperViewQR.h
+//  SuperViewQR
+//
+//  Created by Brommko LLC on 06/06/2019.
+//  Copyright © 2019 Brommko LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SuperViewQR.
+FOUNDATION_EXPORT double SuperViewQRVersionNumber;
+
+//! Project version string for SuperViewQR.
+FOUNDATION_EXPORT const unsigned char SuperViewQRVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SuperViewQR/PublicHeader.h>
+
+
